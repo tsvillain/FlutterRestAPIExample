ccae2cf68a1fa6b218967926e33635a7d0529f48
Tekeshwar Singh <tekeshwarsingh2000@gmail.com>
2020-10-07 11:52:06 +0530

    key removed

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,5 +1,5 @@
 class Constansts {
-  static String apiKey = "4a3045696e154d709475e1f566daf9a9";
+  static String apiKey = "ADD-YOUT-OWN-API-KEY-HERE";
   static String endPoint =
       "https://newsapi.org/v2/top-headlines?country=us&apiKey=$apiKey";
 }
